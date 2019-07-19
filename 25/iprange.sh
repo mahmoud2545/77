e97d3bff7413d7b13391130d9cab20ccb644fe80
mahmoud2545 <52058822+mahmoud2545@users.noreply.github.com>
2019-07-19 22:03:33 +0300

    Add files via upload

---
+++ 25/iprange.sh
@@ -0,0 +1,55 @@
+mkdir scan/
+./r.py 138.59.108.0/22 >> scan/ara.txt
+./r.py 138.122.108.0/22 >> scan/ara.txt
+./r.py 138.185.136.0/22 >> scan/ara.txt
+./r.py 143.255.248.0/22 >> scan/ara.txt
+./r.py 157.100.0.0/16 >> scan/ara.txt
+./r.py 167.250.180.0/22 >> scan/ara.txt
+./r.py 168.194.148.0/22 >> scan/ara.txt
+./r.py 168.197.188.0/22 >> scan/ara.txt
+./r.py 168.197.188.0/22 >> scan/ara.txt
+./r.py 170.82.112.0/22 >> scan/ara.txt
+./r.py 186.178.0.0/16 >> scan/ara.txt
+./r.py 170.82.156.0/22 >> scan/ara.txt
+./r.py 170.83.216.0/22 >> scan/ara.txt
+./r.py 170.238.0.0/22 >> scan/ara.txt
+./r.py 170.238.170.0/23 >> scan/ara.txt
+./r.py 179.0.204.0/24 >> scan/ara.txt
+./r.py 179.49.0.0/18 >> scan/ara.txt
+./r.py 179.60.48.0/20 >> scan/ara.txt
+./r.py 179.60.244.0/22 >> scan/ara.txt
+./r.py 181.39.0.0/16 >> scan/ara.txt
+./r.py 181.112.0.0/15 >> scan/ara.txt
+./r.py 181.175.0.0/16 >> scan/ara.txt
+./r.py 181.188.192.0/18 >> scan/ara.txt
+./r.py 181.196.0.0/16 >> scan/ara.txt
+./r.py 181.199.0.0/17 >> scan/ara.txt
+./r.py 181.198.0.0/16 >> scan/ara.txt
+./r.py 181.211.0.0/16 >> scan/ara.txt
+./r.py 181.224.172.0/22 >> scan/ara.txt
+./r.py 186.3.0.0/16 >> scan/ara.txt
+./r.py 170.239.204.0/22 >> scan/ara.txt
+./r.py 170.254.28.0/22 >> scan/ara.txt
+./r.py 177.234.192.0/18 >> scan/ara.txt
+./r.py 179.0.129.0/24 >> scan/ara.txt
+./r.py 179.0.204.0/24 >> scan/ara.txt
+./r.py 179.49.0.0/18 >> scan/ara.txt
+./r.py 179.60.48.0/20 >> scan/ara.txt
+./r.py 179.60.244.0/22 >> scan/ara.txt
+./r.py 181.39.0.0/16 >> scan/ara.txt
+./r.py 181.112.0.0/15 >> scan/ara.txt
+./r.py 181.175.0.0/16 >> scan/ara.txt
+./r.py 181.188.192.0/18 >> scan/ara.txt
+./r.py 181.199.0.0/17 >> scan/ara.txt
+./r.py 181.196.0.0/16 >> scan/ara.txt
+./r.py 181.198.0.0/16 >> scan/ara.txt
+./r.py 181.211.0.0/16 >> scan/ara.txt
+./r.py 181.224.172.0/22 >> scan/ara.txt
+./r.py 186.3.0.0/16 >> scan/ara.txt
+
+
+
+
+
+
+
